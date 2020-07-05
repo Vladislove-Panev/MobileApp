ca2b5e6d7521c32e6bce60fda0dd0e419b5902b9
Vladislav <vladislovepanev@gmail.com>
2020-07-05 18:24:23 +0300

    add ReverseNumber

---
+++ MobileApp/MobileApp/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using System.Threading;
+
+namespace MobileApp
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnButtonClicked(object sender, EventArgs args)
+        {
+            label1.Text = Convert.ToString(ReverseNumber(Convert.ToInt32(entry.Text)));
+        }
+
+        private int ReverseNumber(int num)
+        {
+            int newNum = 0;
+            
+            while (num != 0)
+            {
+                newNum *= 10;
+                int tmp = num % 10;
+                newNum += tmp;
+                num /= 10;
+            }
+
+
+            return newNum;
+        }
+    }
+}
